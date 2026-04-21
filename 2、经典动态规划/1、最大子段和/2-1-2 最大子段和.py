a88f9c70b76e1d2ca309660019bb94b5a7d69822
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-21 19:16:43 +0800

    模板 + 第一道题目提交

--- 2、经典动态规划/1、最大子段和/2-1-2 最大子段和.py
+++ 2、经典动态规划/1、最大子段和/2-1-2 最大子段和.py
@@ -1,13 +1,18 @@
+#############最大子数组和模板#############
+# a列表的第0个元素是不用的，从第1个元素开始
 def getMSS(n, a, dp):
-    # dp[i] 表示以第 i 个数结尾的最大子段和
-    ans = -10**18
+    # dp[i] 代表以第 i 个数结尾的最大子段和
     dp[0] = 0
     for i in range(1, n+1):
         dp[i] = a[i] + max(dp[i-1], 0)
-        ans = max(ans, dp[i])
-    return ans
+    # 转换成前 i 个元素的最大子段和
+    dp[0] = -10**18
+    for i in range(1, n+1):
+        dp[i] = max(dp[i], dp[i-1])
+    return dp[n]
+#############最大子数组和模板#############
 
 n = int(input())
 a = [0] + list(map(int, input().split()))
-dp = [0] * (n + 1)
+dp = [0] * len(a)
 print(getMSS(n, a, dp))
--- 2、经典动态规划/1、最大子段和/2-1-3-1 区间最大子序列和.py
+++ 2、经典动态规划/1、最大子段和/2-1-3-1 区间最大子序列和.py
@@ -1,29 +1,27 @@
+#############最大子数组和模板#############
+# a列表的第0个元素是不用的，从第1个元素开始
 def getMSS(n, a, dp):
     # dp[i] 代表以第 i 个数结尾的最大子段和
-    ans = -10**18
     dp[0] = 0
     for i in range(1, n+1):
         dp[i] = a[i] + max(dp[i-1], 0)
-        ans = max(ans, dp[i])
     # 转换成前 i 个元素的最大子段和
     dp[0] = -10**18
     for i in range(1, n+1):
         dp[i] = max(dp[i], dp[i-1])
-    return ans
+    return dp[n]
+#############最大子数组和模板#############
 
+### 数据输入
 n = int(input())
 a = [0] + list(map(int, input().split()))
-
-maxn = 2010
-dp = [[0]*(maxn) for _ in range(maxn)]
-
+### 初始化
+dp = [[0]*(n + 1) for _ in range(n + 1)]
+### 预处理
 for i in range(1, n+1):
-    # 构建子数组 a[i...n]
-    sub_a = [0] * (n - i + 2)
-    for j in range(1, n - i + 2):
-        sub_a[j] = a[i + j - 1]
-    getMSS(n - i + 1, sub_a, dp[i])
-
+    sub_a = a[i-1:]
+    getMSS(len(sub_a)-1, sub_a, dp[i])
+### 查询
 t = int(input())
 while t > 0:
     t -= 1
