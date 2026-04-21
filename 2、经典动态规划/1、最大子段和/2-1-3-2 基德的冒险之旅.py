f69778023f2b283d663dc949bdce9251c336a717
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-21 19:23:26 +0800

    最大子段和的剩余题

--- 2、经典动态规划/1、最大子段和/2-1-3-2 基德的冒险之旅.py
+++ 2、经典动态规划/1、最大子段和/2-1-3-2 基德的冒险之旅.py
@@ -1,32 +1,33 @@
+#############最大子数组和模板#############
+# a列表的第0个元素是不用的，从第1个元素开始
 def getMSS(n, a, dp):
     # dp[i] 代表以第 i 个数结尾的最大子段和
-    ans = -10**18
     dp[0] = 0
     for i in range(1, n+1):
         dp[i] = a[i] + max(dp[i-1], 0)
-        ans = max(ans, dp[i])
     # 转换成前 i 个元素的最大子段和
     dp[0] = -10**18
     for i in range(1, n+1):
         dp[i] = max(dp[i], dp[i-1])
-    return ans
+    return dp[n]
+#############最大子数组和模板#############
 
 def swap(n, a):
-    for i in range(1, n//2 + 1):
-        a[i], a[n+1-i] = a[n+1-i], a[i]
+    a[1:n+1] = a[1:n+1][::-1]
 
+### 数据输入
 n, k = map(int, input().split())
 a = [0] + list(map(int, input().split()))
+dppre = [0] * (n+1)
+dppost = [0] * (n+1)
 
-maxn = 100010
-dppre = [0] * maxn
-dppost = [0] * maxn
-
+### 预处理
 getMSS(n, a, dppre)
 swap(n, a)
 getMSS(n, a, dppost)
 swap(n, dppost)
 
+### 计算结果
 ans = -10**18
 for i in range(1, n - k):
     if i + k + 1 <= n:
--- 2、经典动态规划/1、最大子段和/2-1-3-3 可删除元素的最大子数组和.py
+++ 2、经典动态规划/1、最大子段和/2-1-3-3 可删除元素的最大子数组和.py
@@ -1,23 +1,22 @@
+### 数据输入
 n, k = map(int, input().split())
 a = [0] + list(map(int, input().split()))
-
-maxn = 100010
-maxk = 101
-dp = [[-10**9]*(maxk) for _ in range(maxn)]
-
+### 初始化
+dp = [[-10**9]*(k+1) for _ in range(n+1)]
 dp[0][0] = 0
-for j in range(1, k+1):
-    dp[0][j] = -10**9
 
+### 状态转移
 for i in range(1, n+1):
     dp[i][0] = max(dp[i-1][0], 0) + a[i]
     for j in range(1, k+1):
         # 当前这个元素 -     不删除           删除
         dp[i][j] = max(dp[i-1][j] + a[i], dp[i-1][j-1])
 
+### 计算结果
 ans = -10**9
 for i in range(1, n+1):
     for j in range(0, k+1):
         ans = max(ans, dp[i][j])
 
+### 输出结果
 print(ans)
