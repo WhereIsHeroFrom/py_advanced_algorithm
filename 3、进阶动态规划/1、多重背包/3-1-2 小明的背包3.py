a9322489bf0929efa81f5168e84dccee92b81d2c
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-23 08:17:51 +0800

    模板化完成

--- 3、进阶动态规划/1、多重背包/3-1-2 小明的背包3.py
+++ 3、进阶动态规划/1、多重背包/3-1-2 小明的背包3.py
@@ -1,38 +1,37 @@
+######################多重背包模板######################
+inf = -1
+init = 0
 def opt(a, b):
-    if a == -1:
+    if a == inf:
         return b
-    if b == -1:
+    if b == inf:
         return a
     return max(a, b)
 
 def Knapsack01(n, V, w, v, dp):
-    # 1、初始化
     for i in range(1, V+1):
-        dp[i] = -1
-    dp[0] = 0
-    # 2、状态转移
+        dp[i] = inf
+    dp[0] = init
     for i in range(1, n+1):
         for j in range(V, w[i]-1, -1):
-            if dp[j - w[i]] != -1:
+            if dp[j - w[i]] != inf:
                 dp[j] = opt(dp[j], dp[j - w[i]] + v[i])
 
 def KnapsackMultiple(n, V, w, v, c, dp):
-    m = 0  # 拆分以后，物品数量就是 m 了，不再是 n
-    nw = [0] * (n * 10 + 1)  # 最大拆分数为log2(c[i])
-    nv = [0] * (n * 10 + 1)
+    nw, nv = [0], [0]
     for i in range(1, n+1):
         k = 1
         while k < c[i]:
-            m += 1
-            nw[m] = k * w[i]
-            nv[m] = k * v[i]
+            nw.append(k * w[i])
+            nv.append(k * v[i])
             c[i] -= k
             k *= 2
         if c[i] > 0:
-            m += 1
-            nw[m] = c[i] * w[i]
-            nv[m] = c[i] * v[i]
-    Knapsack01(m, V, nw, nv, dp)
+            nw.append(c[i] * w[i])
+            nv.append(c[i] * v[i])
+    Knapsack01(len(nw)-1, V, nw, nv, dp)
+######################多重背包模板######################
+
 
 n, V = map(int, input().split())
 w = [0] * (n + 1)
