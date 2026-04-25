9a4321d7265ec08ffd482509bcaeb4da634d578f
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-25 10:30:09 +0800

    模板化完成

--- 4、字符串/2、manacher/4-2-3-2 小蓝的01串.py
+++ 4、字符串/2、manacher/4-2-3-2 小蓝的01串.py
@@ -1,68 +1,91 @@
-# Manacher算法模板
-MAXN = 1000010
-SPLIT = '$'
-p = [0] * MAXN
-
+################################Manacher算法模板################################
 class Manacher:
+    SPLIT = '$'
     def __init__(self):
+        # p[]: 每个位置的回文半径
+        # ct: 当前已知最右回文区域的中心位置
+        # r: 当前已知最右回文区域的右边界（即ct + p[ct]）
+        self.p = []
+        self.ct = 0
+        self.r = 0
         self.str_tmp = ''
-        self.processed_str = ''
+        self.str_processed = ''
     
     def manacher_pre(self, s):
         self.str_tmp = s
         new_str = []
         for i in range(len(self.str_tmp)):
-            new_str.append(SPLIT)
+            new_str.append(self.SPLIT)
             new_str.append(self.str_tmp[i])
-        new_str.append(SPLIT)
-        self.processed_str = ''.join(new_str)
-        return self.processed_str
+        new_str.append(self.SPLIT)
+        new_str = ''.join(new_str)
+        self.p = [0] * len(new_str)
+        return new_str
     
     def manacher_match(self, a, b):
         if a == b:
-            return a == SPLIT
+            return a == self.SPLIT
         return (int(a) + int(b)) == 1
     
     def manacher(self, s):
-        s = self.manacher_pre(s)
-        # ct: 当前已知最右回文区域的中心位置
-        # r: 当前已知最右回文区域的右边界（即ct + p[ct]）
-        # p[]: 记录每个位置的回文半径
-        ct = 0
-        r = 0
+        self.str_processed = self.manacher_pre(s)
         max_len = 1
-        p[0] = 1
+        self.p[0] = 1
         
-        for i in range(1, len(s)):
+        for i in range(1, len(self.str_processed)):
             # 1.计算p[i]初始值
-            if i < r:
-                p[i] = min(p[2*ct - i], r - i)
+            if i < self.r:
+                self.p[i] = min(self.p[2*self.ct - i], self.r - i)
             else:
-                p[i] = 0
+                self.p[i] = 0
             
             # 2.扩张p[i]，以适应达到p[i]最大值
-            while i - p[i] >= 0 and i + p[i] < len(s) and self.manacher_match(s[i - p[i]], s[i + p[i]]):
-                p[i] += 1
+            while i - self.p[i] >= 0 and i + self.p[i] < len(self.str_processed):
+                if not self.manacher_match(
+                    self.str_processed[i - self.p[i]], 
+                    self.str_processed[i + self.p[i]]
+                ):
+                    break
+                self.p[i] += 1
             
             # 3.更新ct
-            if p[i] + i > r:
-                ct = i
-                r = p[i] + i
+            if self.p[i] + i > self.r:
+                self.ct = i
+                self.r = self.p[i] + i
             
             # 4.更新最长回文
-            if 2 * p[i] - 1 > max_len:
-                max_len = 2 * p[i] - 1
+            if 2 * self.p[i] - 1 > max_len:
+                max_len = 2 * self.p[i] - 1
         
         return max_len
 
+################################Manacher算法模板################################
+
+'''
+1、首先，满足要求的子串一定是偶数长度的。
+反证法证明，假设是奇数，如下：
+原串： a    b    c
+取反：1-a  1-b  1-c
+反转：1-c  1-b  1-a
+你会发现，要求 b == 1-b，显然是不合理的，所以可得，原串必为偶数。
+2、于是，同样的方法， 观察偶数情况：
+原串： a    b    c    d
+取反：1-a  1-b  1-c  1-d
+反转：1-d  1-c  1-b  1-a
+得出结论：a+d==1   b+c==1
+3、这样一来，只需要把原先的 马拉车 模板，两边字符相等的逻辑，改成字符相加等于1即可
+4、最后统计这样的偶数串的个数，做简单的计数操作即可
+偶数串的中心位置一定是$，所以p[i] // 2 就是偶数串的个数
+'''
+
+
 manacher = Manacher()
 n = int(input())
-s = input().strip()
+s = input()
 manacher.manacher(s)
 
 ans = 0
-# 遍历处理后的字符串中的分隔符位置（偶数索引）
-for i in range(0, len(manacher.processed_str), 2):
-    ans += p[i] // 2
+for i in range(0, len(manacher.str_processed), 2):
+    ans += manacher.p[i] // 2
 
 print(ans)
--- 4、字符串/2、manacher/4-2-3-3 反异或01串.py
+++ 4、字符串/2、manacher/4-2-3-3 反异或01串.py
@@ -1,57 +1,82 @@
-# Manacher算法模板
-MAXN = 2000010
-SPLIT = '$'
-p = [0] * MAXN
-
+################################Manacher算法模板################################
 class Manacher:
+    SPLIT = '$'
     def __init__(self):
+        # p[]: 每个位置的回文半径
+        # ct: 当前已知最右回文区域的中心位置
+        # r: 当前已知最右回文区域的右边界（即ct + p[ct]）
+        self.p = []
+        self.ct = 0
+        self.r = 0
         self.str_tmp = ''
-        self.processed_str = ''
+        self.str_processed = ''
     
     def manacher_pre(self, s):
         self.str_tmp = s
         new_str = []
         for i in range(len(self.str_tmp)):
-            new_str.append(SPLIT)
+            new_str.append(self.SPLIT)
             new_str.append(self.str_tmp[i])
-        new_str.append(SPLIT)
-        self.processed_str = ''.join(new_str)
-        return self.processed_str
+        new_str.append(self.SPLIT)
+        new_str = ''.join(new_str)
+        self.p = [0] * len(new_str)
+        return new_str
     
     def manacher_match(self, a, b):
         return a == b
     
     def manacher(self, s):
-        s = self.manacher_pre(s)
-        # ct: 当前已知最右回文区域的中心位置
-        # r: 当前已知最右回文区域的右边界（即ct + p[ct]）
-        # p[]: 记录每个位置的回文半径
-        ct = 0
-        r = 0
+        self.str_processed = self.manacher_pre(s)
         max_len = 1
-        p[0] = 1
+        self.p[0] = 1
         
-        for i in range(1, len(s)):
+        for i in range(1, len(self.str_processed)):
             # 1.计算p[i]初始值
-            if i < r:
-                p[i] = min(p[2*ct - i], r - i)
+            if i < self.r:
+                self.p[i] = min(self.p[2*self.ct - i], self.r - i)
             else:
-                p[i] = 0
+                self.p[i] = 0
             
             # 2.扩张p[i]，以适应达到p[i]最大值
-            while i - p[i] >= 0 and i + p[i] < len(s) and self.manacher_match(s[i - p[i]], s[i + p[i]]):
-                p[i] += 1
+            while i - self.p[i] >= 0 and i + self.p[i] < len(self.str_processed):
+                if not self.manacher_match(
+                    self.str_processed[i - self.p[i]], 
+                    self.str_processed[i + self.p[i]]
+                ):
+                    break
+                self.p[i] += 1
             
             # 3.更新ct
-            if p[i] + i > r:
-                ct = i
-                r = p[i] + i
+            if self.p[i] + i > self.r:
+                self.ct = i
+                self.r = self.p[i] + i
             
             # 4.更新最长回文
-            if 2 * p[i] - 1 > max_len:
-                max_len = 2 * p[i] - 1
+            if 2 * self.p[i] - 1 > max_len:
+                max_len = 2 * self.p[i] - 1
         
         return max_len
+
+################################Manacher算法模板################################
+
+
+'''
+1、由于反异或操作只能进行最多一次，所以我们就可以挑选其中一段来进行反异或操作，
+而剩下的段，统计 1 的个数就可以了，于是问题就转变成了 "如何找到需要反异或操作的段"；
+
+2、考虑这种情况，先考虑奇数的情况：
+a  b  c  d  e
+^  ^  ^  ^  ^
+e  d  c  b  a
+满足可以反异或的前提：中间这个位置必须是 0，因为自己异或自己必然是 0
+然后由于异或满足交换律，也就是 b^d == d^b，所以就变成了求回文串的过程了；
+同样偶数也是一样的，只不过偶数不需要判断中间位置是 0 的情况；
+
+3、利用 manacher 计算出每个位置作为回文串中心的最大半径 p[i]，
+并且求 区间中[ i, i+p[i]-1 ] 中 1 的个数，代表 反异或 操作之前需要的 1
+再求区间 [i-p[i]+1, i+p[i]-1] 以外的1 的个数代表不进行反异或操作的段中需要的 1；
+把两者累加即可；
+'''
 
 def getsum(sum_arr, l, r):
     pre = 0
@@ -63,7 +88,7 @@ manacher = Manacher()
 s = input().strip()
 manacher.manacher(s)
 
-processed_str = manacher.processed_str
+processed_str = manacher.str_processed
 n = len(processed_str)
 
 # 计算前缀和
@@ -84,8 +109,10 @@ for i in range(n):
         pass
     
     # 统计 [i, i+p[i]-1] 的 1 的个数
+    ans = getsum(sum_arr, i, i + manacher.p[i] - 1) 
     # 再统计这个回文串 [i-p[i]+1, i+p[i]-1] 以外1的个数
-    ans = getsum(sum_arr, i, i + p[i] - 1) + getsum(sum_arr, 0, n-1) - getsum(sum_arr, i - p[i] + 1, i + p[i] - 1)
+    ans += getsum(sum_arr, 0, n-1) 
+    ans -= getsum(sum_arr, i - manacher.p[i] + 1, i + manacher.p[i] - 1)
     ret = min(ret, ans)
 
 print(ret)
